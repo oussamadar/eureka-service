fb56c9f58c487c5bbe24854f6d7a4e097b454b60
dariaoui <oussama.darioui@gmail.com>
2021-01-15 17:23:17 +0100

    first commit

---
+++ src/test/java/org/sid/eurekaserviceoldvers/EurekaServiceOldversApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.eurekaserviceoldvers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceOldversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
